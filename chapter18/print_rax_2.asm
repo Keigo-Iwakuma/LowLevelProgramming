23f0cacf895c48ad4e50757a103843bf86f04fb6
Keigo-Iwakuma <sym90chn@gmail.com>
2021-12-03 23:58:25 +0900

    update

---
+++ chapter18/print_rax_2.asm
@@ -0,0 +1,17 @@
+section .data
+codes:
+    db '0123456789ABCDEF'
+
+section .text
+global _start
+_start:
+    mov rax, 0x1122334455667788
+    mov rdi, 1
+    mov rdx, 1
+    mov rcx, 64
+
+.loop:
+    push rax
+    sub rcx, 4
+    sar rax, cl
+    and rax, 0xf
